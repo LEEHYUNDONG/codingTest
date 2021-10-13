26d29bfa228921433e8ee3c43e2aecf25a125bed
LEEHYUNDONG <l.steve10@yahoo.com>
2021-10-13 14:30:03 +0900

    봄버맨

--- BOJ/BOJ_16918.py
+++ BOJ/BOJ_16918.py
@@ -1,13 +1,14 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 r, c, n = map(int, input().split())
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 bomb = []
-
-final = [['.' for _ in range(c)] for _ in range(r)]
 data = []
+new_bomb = []
+final = [['.' for _ in range(c)]]*r
 
 for i in range(r):
     tmp = input().rstrip()
@@ -18,17 +19,51 @@ for i in range(r):
 
 cnt = 0
 time = n
-while cnt < time:
-    if cnt % 3 == 0:
-        cnt += 1
-        continue
-    elif cnt % 3 == 1:
-        for i in range(r):
-            for j in range(c):
-                if data[i][j] != 'O':
-                    new_bomb.append([i, j])
-    elif cnt % 3 == 2:
-        while bomb:
-            x, y = bomb.popleft()
 
-    cnt += 1
+# def allBombSet():
+#     for i in range(r):
+#         for j in range(c):
+
+
+# def bfs(b):
+#     q = deque(b)
+#     while q:
+#         x, y = q.popleft()
+#         for i in range(4):
+#             rx, ry = x + dx[i], y + dy[i]
+#             if 0 <= rx < r and 0 <= ry < c:
+#                 if data[rx][ry] == '.':
+#                     data[rx][ry] = 'O'
+
+
+if n == 1:
+    for i in range(r):
+        print('O'*c)
+
+elif n % 2 == 0:
+    for i in range(r):
+        print('O'*c)
+
+elif n % 4 == 1 and n > 1:
+    for i in range(r):
+        for j in range(c):
+            print(data[i][j], end='')
+        print()
+
+else:
+    q = deque(bomb)
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            rx, ry = x + dx[i], y + dy[i]
+            if 0 <= rx < r and 0 <= ry < c:
+                if data[rx][ry] == 'O':
+                    data[rx][ry] = '.'
+
+    for i in range(r):
+        for j in range(c):
+            if data[i][j] == '.':
+                print("O", end='')
+            else:
+                print(".", end='')
+        print()
