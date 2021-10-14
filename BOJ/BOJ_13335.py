273c04e867cca2a9fe0b47d9afac39a44791c5ba
LEEHYUNDONG <l.steve10@yahoo.com>
2021-10-14 17:14:52 +0900

    트럭

--- BOJ/BOJ_13335.py
+++ BOJ/BOJ_13335.py
@@ -4,15 +4,20 @@ input = sys.stdin.readline
 
 n, w, L = map(int, input().split())
 truck = list(map(int, input().split()))
-cnt = 0
+trucks = deque(truck)
+dep = deque()
+bridge = deque()
+time = 0
 
-if sum(truck) <= w:
-    print(w+n)
-else:
-    l = []
-    for i in range(n):
-        for j in range(i):
-            if sum(truck[j:i+1]) <= L:
-                cnt += 1
-                l.append(i-j+1)
-    print((w+1)*n - w*(cnt+1))
+while bridge or trucks:
+    time += 1
+    if bridge:  # 다리에 트럭ㅣ 있을때
+        if dep[0] + w == time:  # 들어간 시간에서 w를 더하면 나올 시간
+            bridge.popleft()
+            dep.popleft()
+    if trucks:
+        if sum(bridge) + trucks[0] <= L:
+            bridge.append(trucks.popleft())
+            dep.append(time)
+
+print(time)
