e7f7dec6768c1e9159450fbee9fff3674c8ea4a9
LEEHYUNDONG <l.steve10@yahoo.com>
2021-10-19 15:46:30 +0900

    Longest Substring without repeating

---
+++ leetCode/leet_LSW.py
@@ -0,0 +1,29 @@
+from collections import deque
+s = "aaaaaddfsfdaef"
+
+index = 0
+length = 0
+cnt = 0
+q = deque(list(s))
+res = deque([])
+# for i in range(len(s)-1):
+#     if s[i] != s[i+1] and s[i] not in s[index:i]:
+#         cnt += 1
+#     elif s[i] in s[index:i+1]:
+#         index = i
+#         cnt = 0
+#     length = max(cnt, length)
+
+while q:
+    tmp = q.popleft()
+    if tmp not in res:
+        res.append(tmp)
+    elif tmp in res:
+        # print(2)
+        length = max(len(res), length)
+        while tmp in res:
+            res.popleft()
+        res.append(tmp)
+
+length = max(len(res), length)
+print(length)
