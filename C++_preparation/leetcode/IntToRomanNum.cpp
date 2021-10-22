ebbb5c41b9909ee6ab198c1a2229693568b16622
LEEHYUNDONG <l.steve10@yahoo.com>
2021-10-22 17:34:29 +0900

    integer to roman number

---
+++ C++_preparation/leetcode/IntToRomanNum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        string res;
+        vector<int> n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> s = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int i = 0;
+        while (num)
+        {
+            int r = num / n[i];
+            while (r--)
+                res += s[i];
+            num = num % n[i];
+            i++;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.intToRoman(2001) << endl;
+
+    return 0;
+}
---
+++ C++_preparation/leetcode/leetcode_containerWithMostWater_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int maxCont = INT_MIN;
+
+        int l = 0;
+        int r = height.size() - 1;
+
+        while (l < r)
+        {
+
+            int len = r - l;
+            maxCont = max(maxCont, len * min(height[l], height[r]));
+
+            if (height[l] > height[r])
+                r--;
+            else`
+                l++;
+        }
+
+        return maxCont;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> tmp;
+    tmp.push_back(2);
+    tmp.push_back(3);
+    tmp.push_back(4);
+    tmp.push_back(5);
+    tmp.push_back(18);
+    tmp.push_back(17);
+    tmp.push_back(6);
+    cout << s.maxArea(tmp) << '\n';
+
+    return 0;
+}
