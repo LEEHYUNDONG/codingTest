59e340d4cdbf56f3b0a693ccce21ef6b0cf95bc8
LEEHYUNDONG <l.steve10@yahoo.com>
2021-11-07 21:35:27 +0900

    벽 부수고 이동하기

--- C++_preparation/BOJ/BOJ_breakWallAndMove.cpp
+++ C++_preparation/BOJ/BOJ_breakWallAndMove.cpp
@@ -5,18 +5,19 @@
 
 using namespace std;
 
-int visited[1001][1001];
+int visited[1001][1001][2];
 int dist[1001][1001];
 
 int main()
 {
     int n, m;
     cin >> n >> m;
-    int dx[4] = {0, 0, -1, 1};
+    int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
-    queue<pair<pair<int, int>, int> > q;
+    queue<pair<pair<int, int>, pair<int, int> > > q;
+
     memset(visited, 0, sizeof(visited));
-    memset(dist, 1000001, sizeof(dist));
+    // memset(dist, 1000001, sizeof(dist));
 
     vector<string> map(n);
     for (int i = 0; i < n; i++)
@@ -24,18 +25,18 @@ int main()
         cin >> map[i];
     }
 
-    visited[0][0] = 1;
-    q.push({{0, 0}, 1});
-    dist[0][0] = 1;
+    visited[0][0][0] = 1;
+    q.push({{0, 0}, {0, 1}});
 
     while (!q.empty())
     {
         int x = q.front().first.first, y = q.front().first.second;
-        int block = q.front().second;
+        int block = q.front().second.first;
+        int cnt = q.front().second.second;
         q.pop();
         if (x == n - 1 && y == m - 1)
         {
-            cout << dist[n - 1][m - 1] << endl;
+            cout << cnt << endl;
             return 0;
         }
         for (int i = 0; i < 4; i++)
@@ -43,18 +44,15 @@ int main()
             int rx = x + dx[i], ry = y + dy[i];
             if (rx < 0 || rx >= n || ry < 0 || ry >= m)
                 continue;
-            if (map[rx][ry] == '1' && block)
+            if (map[rx][ry] == '1' && !block)
             {
-
-                q.push({{rx, ry}, 0});
-                visited[rx][ry] = 1;
-                dist[rx][ry] = dist[x][y] + 1;
+                visited[rx][ry][block + 1] = 1;
+                q.push({{rx, ry}, {block + 1, cnt + 1}});
             }
-            if (map[rx][ry] == '0' && visited[rx][ry] == 0)
+            else if (map[rx][ry] == '0' && visited[rx][ry][block] == 0)
             {
-                q.push({{rx, ry}, 0});
-                visited[rx][ry] = 1;
-                dist[rx][ry] = dist[x][y] + 1;
+                visited[rx][ry][block] = 1;
+                q.push({{rx, ry}, {block, cnt + 1}});
             }
         }
     }
