f0341d2fc80aa42bb57428716a2a852817e5eb86
LEEHYUNDONG <l.steve10@yahoo.com>
2021-11-15 17:57:07 +0900

    종이의 수

---
+++ C++_preparation/BOJ/BOJ_numberOfPaper.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <memory.h>
+
+using namespace std;
+
+int ans[3] = {0, 0, 0};
+int data[3001][3001];
+
+void cut(int x, int y, int size)
+{
+    bool flag = true;
+    for (int i = x; i < x + size; i++)
+    {
+        for (int j = y; j < y + size; j++)
+        {
+            if (data[x][y] != data[i][j])
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if (flag)
+    {
+        ans[data[x][y] + 1]++;
+        return;
+    }
+
+    for (int i = x; i < x + size; i += size / 3)
+    {
+        for (int j = y; j < y + size; j += size / 3)
+        {
+            cut(i, j, size / 3);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> tmp;
+            data[i][j] = tmp;
+        }
+    }
+
+    cut(0, 0, n);
+
+    for (auto a : ans)
+        cout << a << '\n';
+    return 0;
+}
