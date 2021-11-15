2b1ee04fb5822b1b0b4bdca34c8e2299fab7e89c
LEEHYUNDONG <l.steve10@yahoo.com>
2021-11-15 19:40:31 +0900

    종이의 개수 복습

---
+++ C++_preparation/BOJ/BOJ_numberOfPaper_2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int data[3001][3001];
+int ans[3] = {0, 0, 0};
+
+void cut(int x, int y, int size)
+{
+    bool flag = true;
+    for (int i = x; i < x + size; i++)
+    {
+        for (int j = y; j < y + size; j++)
+        {
+            if (data[i][j] != data[x][y])
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if (flag)
+    {
+        ans[data[x][y] + 1]++;
+        return;
+    }
+    for (int i = x; i < x + size; i += size / 3)
+    {
+        for (int j = y; j < y + size; j += size / 3)
+        {
+            cut(i, j, size / 3);
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> data[i][j];
+        }
+    }
+    cut(0, 0, n);
+    for (auto a : ans)
+    {
+        cout << a << '\n';
+    }
+    return 0;
+}
