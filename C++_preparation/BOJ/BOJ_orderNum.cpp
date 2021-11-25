82d8bbcf8edea5a380c00357a4659c49255178fd
LEEHYUNDONG <l.steve10@yahoo.com>
2021-11-25 10:22:06 +0900

    수 정렬하기 2

---
+++ C++_preparation/BOJ/BOJ_orderNum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    for (auto a : arr)
+    {
+        cout << a << '\n';
+    }
+    return 0;
+}
