f5343d8fa053b0ec16cf9e9a35d59b18a84a53f7
LEEHYUNDONG <l.steve10@yahoo.com>
2021-11-27 16:00:54 +0900

    프린터 큐

--- C++_preparation/BOJ/BOJ_printQueue.cpp
+++ C++_preparation/BOJ/BOJ_printQueue.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
-
 using namespace std;
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int t, n;
-    cin >> t;
-
-    while (t--)
+    int count = 0;
+    int test_case;
+    cin >> test_case;
+    int n, m, ipt; //문서의 개수, 궁금한 문서 위치, 중요도
+    for (int i = 0; i < test_case; ++i)
     {
-        int search;
-        priority_queue<pair<int, int> > q;
-        cin >> n >> search;
-        int p;
-        for (int i = 0; i < n; i++)
+        count = 0;
+        cin >> n >> m;
+        queue<pair<int, int> > q;
+        priority_queue<int> pq; // 우선순위 큐
+        for (int j = 0; j < n; ++j)
         {
-            cin >> p;
-            q.push(make_pair(p, i));
+            cin >> ipt;
+            q.push({j, ipt});
+            pq.push(ipt);
         }
-
-        for (int i = 0; i < n; i++)
+        while (!q.empty())
         {
-            auto a = q.top();
+            int index = q.front().first;
+            int value = q.front().second;
             q.pop();
-            cout << a.second << endl;
-            if (a.second == search)
+            if (pq.top() == value)
             {
-                cout << i + 1 << '\n';
-                break;
+                pq.pop();
+                ++count;
+                if (index == m)
+                {
+                    cout << count << endl;
+                    break;
+                }
             }
+            else
+                q.push({index, value});
         }
     }
     return 0;
