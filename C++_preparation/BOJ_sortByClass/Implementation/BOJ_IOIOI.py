c9ea74c5f20eef95c971172c35783cb78f152464
LEEHYUNDONG <l.steve10@yahoo.com>
2021-12-20 12:45:13 +0900

    IOIOI 해답

---
+++ C++_preparation/BOJ_sortByClass/Implementation/BOJ_IOIOI.py
@@ -0,0 +1,17 @@
+N = int(input())
+M = int(input())
+string = input()
+index = 1
+answer = 0
+pattern = 0
+while index < M-1:
+    if string[index-1] == 'I' and string[index] == 'O' and string[index+1] == 'I':
+        pattern += 1
+        if pattern == N:
+            answer += 1
+            pattern -= 1
+        index += 1
+    else:
+        pattern = 0
+    index += 1
+print(answer)
