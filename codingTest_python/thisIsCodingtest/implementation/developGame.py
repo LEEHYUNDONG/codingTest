229b07faa870c7232130fb6436f8ea94954e45e1
LEEHYUNDONG <l.steve10@yahoo.com>
2021-12-27 14:43:37 +0900

    게임구현

---
+++ codingTest_python/thisIsCodingtest/implementation/developGame.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+visited = [[0]*m for _ in range(n)]
+direction = 0
+r, c, direction = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+q = deque([[r, c, direction]])
+cnt = 0
+graph[r][c] = 1
+while q:
+    x, y, d = q.popleft()
+    flag = 0
+    for i in range(1, 5):
+        rx = dx[(d-i) % 4] + x
+        ry = dy[(d-i) % 4] + y
+        flag += 1
+        if 0 <= rx < n and 0 <= ry < m and graph[rx][ry] == 0:
+            graph[rx][ry] = 1
+            q.append([rx, ry, (d-i) % 4])
+            flag -= 1
+            cnt += 1
+    if flag == 4:
+        x -= dx[(d-i) % 4]
+        y -= dy[(d-i) % 4]
+        if graph[x][y] == 1:
+            break
+        q.append([x, y, (d-i) % 4])
+
+print(cnt)
