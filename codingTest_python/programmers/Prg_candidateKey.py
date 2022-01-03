eb8ced391b0a449779413b2125ab118ca4552947
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-03 13:18:48 +0900

    프로그래머스/후보키

--- codingTest_python/programmers/Prg_candidateKey.py
+++ codingTest_python/programmers/Prg_candidateKey.py
@@ -2,22 +2,22 @@ from itertools import combinations
 
 
 def solution(relation):
-    row = len(relation)
-    col = len(relation[0])
+    answer = 0
+    r = len(relation)
+    c = len(relation[0])
 
-    #가능한 속성의 모든 인덱스 조합
     comb = []
-    for i in range(1, col+1):
-        comb.extend(combinations(range(col), i))
+    for i in range(1, c+1):
+        comb.extend(combinations(range(c), i))
 
-    # unique
     unique = []
     for i in comb:
         tmp = [tuple([item[key] for key in i]) for item in relation]
-        if len(set(tmp)) == row:    # 유일성
+
+        if len(set(tmp)) == r:
             flag = True
             for x in unique:
-                if set(x).issubset(set(i)):  # 최소성
+                if set(x).issubset(set(i)):
                     flag = False
                     break
             if flag:
