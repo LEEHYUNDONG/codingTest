99265409529ccdc9900490e827adf253d9f54747
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-03 12:00:47 +0900

    프로그래머스/후보키

--- codingTest_python/programmers/Prg_candidateKey.py
+++ codingTest_python/programmers/Prg_candidateKey.py
@@ -6,24 +6,21 @@ def solution(relation):
     col = len(relation[0])
 
     #가능한 속성의 모든 인덱스 조합
-    combi = []
+    comb = []
     for i in range(1, col+1):
-        combi.extend(combinations(range(col), i))
+        comb.extend(combinations(range(col), i))
 
-    #유일성
+    # unique
     unique = []
-    for i in combi:
+    for i in comb:
         tmp = [tuple([item[key] for key in i]) for item in relation]
-
         if len(set(tmp)) == row:    # 유일성
-            put = True
-
+            flag = True
             for x in unique:
                 if set(x).issubset(set(i)):  # 최소성
-                    put = False
+                    flag = False
                     break
-
-            if put:
+            if flag:
                 unique.append(i)
 
     return len(unique)
