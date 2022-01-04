c9d4a15acd82027f6be4da1cb5a8b1bb7501b44f
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-04 11:48:27 +0900

    BOJ/n과 m 8

---
+++ codingTest_python/BOJ/BOJ_nAndM_8.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+ans = []
+answer = []
+visited = [False] * (n+1)
+
+
+def dfs(idx, depth):
+    if depth == m:
+        ans.append(num[idx])
+        for i in ans:
+            print(i, end=' ')
+        print()
+        # 하나씩 pop 시켜놔야 방문하지 않았을 때 개수가 맞다.
+        ans.pop()
+        return
+    for i in range(idx, len(num)):
+        ans.append(num[idx])
+        dfs(i, depth+1)
+        ans.pop()
+
+
+num.sort()
+for i in range(n):
+    dfs(i, 1)
