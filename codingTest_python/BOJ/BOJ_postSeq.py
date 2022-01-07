8152ee954449b26fcb4b731480397d4654ab0cff
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-07 10:12:38 +0900

    BOJ/다음 순열

---
+++ codingTest_python/BOJ/BOJ_postSeq.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num = list(map(int, input().split()))
+flag = False
+
+for i in range(n-1, 0, -1):
+    if num[i-1] > num[i]:
+        for j in range(n-1, 0, -1):
+            if num[i-1] > num[j]:
+                num[i-1], num[j] = num[j], num[i-1]
+                num = num[:i] + sorted(num[i:], reverse=True)
+                flag = True
+                break
+    if flag:
+        print(*num)
+        break
+if not flag:
+    print(-1)
