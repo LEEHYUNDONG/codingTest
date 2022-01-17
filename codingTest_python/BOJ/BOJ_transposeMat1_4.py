541b761465b4a968b341f669e17391235951507e
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-17 10:01:37 +0900

    배열 돌리기1

---
+++ codingTest_python/BOJ/BOJ_transposeMat1_4.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+for _ in range(r):
+    for i in range(min(n, m)//2):
+        x, y = i, i
+        tmp = arr[x][y]
+        for j in range(i+1, n-i):
+            x = j
+            prev = arr[x][y]
+            arr[j][y] = tmp
+            tmp = prev
+        for j in range(i+1, m-i):
+            y = j
+            prev = arr[x][y]
+            arr[x][y] = tmp
+            tmp = prev
+
+        for j in range(i+1, n-i):
+            x = n-j-1
+            prev = arr[x][y]
+            arr[x][y] = tmp
+            tmp = prev
+        for j in range(i+1, m-i):
+            y = m-j-1
+            prev = arr[x][y]
+            arr[x][y] = tmp
+            tmp = prev
+
+for i in range(n):
+    print(*arr[i])
