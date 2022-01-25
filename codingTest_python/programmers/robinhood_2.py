2f64aa808144c6d4c73f5f885bad65a65a4fe17d
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-25 11:50:05 +0900

    양궁대회 솔루션 중복 있이 돈다. 완전탐색.

---
+++ codingTest_python/programmers/robinhood_2.py
@@ -0,0 +1,31 @@
+from itertools import combinations_with_replacement
+
+
+def solution(n, info):
+    answer = [0 for _ in range(11)]
+    win = False
+    max_num = 0   # 라이언이 이길때 가장큰 점수 차이
+    # 1. 중복 조합을 이용해 라이언의 점수를 만든다.
+    for res in list(combinations_with_replacement(range(0, 11), n)):
+        now = [0 for _ in range(11)]
+        for r in res:
+            now[10 - r] += 1
+        lion = 0
+        peach = 0
+
+        for i, (l, p) in enumerate(zip(now, info)):
+            if l == p == 0:
+                continue
+            if p >= l:
+                peach += (10 - i)
+            elif l > p:
+                lion += (10 - i)
+
+        if lion > peach:
+            win = True
+            if (lion - peach) > max_num:
+                max_num = lion - peach
+                answer = now
+    if not win:
+        return [-1]
+    return answer
