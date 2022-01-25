e6f5d6c242a9335920bbcffe6b1babddd8eaee4b
LEEHYUNDONG <l.steve10@yahoo.com>
2022-01-25 11:44:57 +0900

    양궁대회 82점짜리

--- codingTest_python/programmers/robinhood.py
+++ codingTest_python/programmers/robinhood.py
@@ -9,27 +9,36 @@ def solution(n, info):
     for k in range(1, n):
         for i in combinations([i for i in range(11)], k):
             ans = [0 for _ in range(11)]
-            arrow = n
-            for j in range(len(info)):
-                if j in i:
-                    ans[j] = info[j]+1
-
-            if sum(ans) == n:
-                apeach, lion = 0, 0
-                for j in range(11):
-                    if info[j] == 0 and ans[j] == 0:
-                        continue
-                    elif info[j] >= ans[j]:
-                        apeach += 10-j
-                    else:
-                        lion += 10-j
-                if apeach < lion:
-                    score.append([idx, lion-apeach])
-                    answer.append(ans)
-                    idx += 1
+            for j in i:
+                ans[j] = info[j]+1
+            for j in range(10, -1, -1):
+                if j not in i and sum(ans) < n and info[j] > 1 and ans[j] == 0:
+                    ans[j] = info[j]-1
+                if sum(ans) == n:
+                    apeach, lion = 0, 0
+                    for r in range(11):
+                        if info[r] == 0 and ans[r] == 0:
+                            continue
+                        elif info[r] >= ans[r]:
+                            apeach += 10-r
+                        else:
+                            lion += 10-r
+                    if apeach < lion:
+                        if sum(ans) == n and apeach < lion:
+                            score.append([idx, lion-apeach])
+                            answer.append(ans)
+                            idx += 1
 
     score.sort(key=lambda x: x[1], reverse=True)
     if len(score) >= 1:
-        return answer[score[0][0]]
+        idx = 0
+        maxV = score[0][1]
+        for i in score:
+            if i[1] == maxV:
+                idx = i[0]
+                continue
+            else:
+                break
+        return answer[idx]
     else:
         return [-1]
