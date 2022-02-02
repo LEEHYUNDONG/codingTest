c449783157228bdb3f5e84573960af78bb2c6616
LEEHYUNDONG <l.steve10@yahoo.com>
2022-02-02 09:34:39 +0900

    빛의 경로의 사이클

--- codingTest_python/programmers/lightCycle.py
+++ codingTest_python/programmers/lightCycle.py
@@ -1,55 +1,3 @@
-# def turnTow(turn, to):
-#     if to == 'L':
-#         turn += 1
-#     elif to == 'R':
-#         turn -= 1
-#     return turn % 4
-
-
-# def solution(grid):
-#     answer = []
-#     direction = [(0, 1), (-1, 0), (0, -1), (1, 0)]  # 우상좌하
-#     turn = 0
-#     x, y = 0, 0
-#     cnt = 0
-#     sizeX, sizeY = len(grid), len(grid[0])
-
-#     for i in range(len(grid)):
-#         grid[i] = list(grid[i])
-
-#     ans = []
-#     visited = [[False] * len(grid[0]) for _ in range(len(grid))]
-#     for i in range(4):
-#         cnt = 0
-#         turn = i
-#         x, y = 0, 0
-#         sx, sy = direction[turn][0], direction[turn][1]
-#         start = grid[x][y]
-#         way = ''
-#         tmp = []
-#         if not visited[x][y]:
-#             while True:
-#                 x = (x + direction[turn][0]) % sizeX
-#                 y = (y + direction[turn][1]) % sizeY
-#                 turn = turnTow(turn, grid[x][y])
-#                 visited[x][y] = True
-#                 cnt += 1
-#                 tmp.append((grid[x][y], x, y))
-#                 if grid[x][y] == start and direction[turn][0] == sx and direction[turn][1] == sy:
-#                     if tmp not in ans:
-#                         ans.append(tmp)
-#                     break
-
-#     if sizeX == 1 and sizeY == 1:
-#         return [1, 1, 1, 1]
-#     else:
-
-#         for i in ans:
-#             # print(i)
-#             answer.append(len(i))
-#         answer.sort()
-#     return answer
-
 def turnTow(turn, to):
     if to == 'L':
         turn += 1
@@ -61,8 +9,6 @@ def turnTow(turn, to):
 def solution(grid):
     answer = []
     direction = [(0, 1), (-1, 0), (0, -1), (1, 0)]  # 우상좌하
-    turn = 0
-    x, y = 0, 0
     cnt = 0
     sizeX, sizeY = len(grid), len(grid[0])
 
@@ -76,11 +22,11 @@ def solution(grid):
             for i in range(4):
                 if visited[x][y][i]:
                     continue
-
+                # print(x, y, visited)
                 cnt = 0
                 rx, ry, turn = x, y, i
-                start = grid[rx][ry]
                 while True:
+                    print(rx, ry, turn)
                     visited[rx][ry][turn] = True
                     rx = (rx + direction[turn][0]) % sizeX
                     ry = (ry + direction[turn][1]) % sizeY
@@ -88,6 +34,7 @@ def solution(grid):
                     cnt += 1
                     if turn == i and rx == x and ry == y:
                         break
+                print('----------')
                 answer.append(cnt)
 
     answer.sort()
