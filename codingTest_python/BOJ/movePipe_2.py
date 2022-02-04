e9137f34082a8276788b83d11331c3ba437b56ad
LEEHYUNDONG <l.steve10@yahoo.com>
2022-02-04 12:06:49 +0900

    파이프 옮기기

--- codingTest_python/BOJ/movePipe_2.py
+++ codingTest_python/BOJ/movePipe_2.py
@@ -8,6 +8,7 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 # 0: 가로로 온 수 / 1: 세로로 온 수 / 2: 대각선으로 온 수
 dp = [[[0] * n for _ in range(n)] for _ in range(3)]
 dp[0][0][1] = 1
+# 가로 미리 채워넣기 첫 번째 행
 i = 2
 while i < n:
     if arr[0][i]:
@@ -15,12 +16,13 @@ while i < n:
     dp[0][0][i] = 1
     i += 1
 
+# 두번 째 행부터 진행
 for i in range(1, n):
     for j in range(2, n):
         if arr[i][j] == 0 and arr[i][j - 1] == 0 and arr[i - 1][j] == 0:
             dp[2][i][j] = sum(dp[k][i - 1][j - 1] for k in range(3))
         if arr[i][j] == 0:
-            dp[0][i][j] = dp[0][i][j - 1] + dp[2][i][j - 1]
-            dp[1][i][j] = dp[1][i - 1][j] + dp[2][i - 1][j]
+            dp[0][i][j] = dp[0][i][j - 1] + dp[2][i][j - 1] # 대각선과 가로로 부터 온것 더함
+            dp[1][i][j] = dp[1][i - 1][j] + dp[2][i - 1][j] # 대각선과 세로로부터 온것 더함
 print(dp)
 print(sum(dp[i][-1][-1] for i in range(3)))
