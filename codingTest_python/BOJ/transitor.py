f605bf2cbf1e583dea17d49aa2c59e3584a8a6dd
LEEHYUNDONG <l.steve10@yahoo.com>
2022-02-10 14:46:47 +0900

    반도체 설계

---
+++ codingTest_python/BOJ/transitor.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+port = list(map(int, input().split()))
+
+dp = [1]*(n+1)
+
+for i in range(1, n):
+    for j in range(i):
+        if port[i] > port[j] and dp[i] < dp[j]+1:
+            dp[i] = dp[j]+1
+
+print(max(dp))
