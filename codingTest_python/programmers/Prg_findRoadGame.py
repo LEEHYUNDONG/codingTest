390ef0fc4042240fa938f29187c000e655433703
LEEHYUNDONG <l.steve10@yahoo.com>
2022-02-14 11:17:31 +0900

    길찾기 게임

--- codingTest_python/programmers/Prg_findRoadGame.py
+++ codingTest_python/programmers/Prg_findRoadGame.py
@@ -1,24 +1,71 @@
-#길찾기 게임
+import sys
+sys.setrecursionlimit(10**6)
+class Node:
+    def __init__(self, x, y, item):
+        self.key = item
+        self.x = x
+        self.y = y
+        self.left = None
+        self.right = None
+
+class binaryTree:
+    def __init__(self):
+        self.root = None
+    
+    def insertNode(self, node, new_node):
+        if node == None:
+            node = new_node
+            return node
+        else:
+            if new_node.x < node.x:
+                node.left = self.insertNode(node.left, new_node)
+            else:
+                node.right = self.insertNode(node.right, new_node)
+            return node
+        
+    def preorder(self, node):
+        global tmp
+        if node == None:
+            return
+        tmp.append(node.key)
+        self.preorder(node.left)
+        self.preorder(node.right)
+        
+    
+    def postorder(self, node):
+        global tmp
+        if node == None:
+            return 
+        self.postorder(node.left)
+        self.postorder(node.right)
+        tmp.append(node.key)
+        
+
+tmp = []
+
 def solution(nodeinfo):
-    answer = [[]]
-    n = len(nodeinfo)
-    graph = [[] for _ in range(n)]
+    global tmp
+    answer = []
+    
+    for i in range(len(nodeinfo)):     
+        nodeinfo[i].extend([i+1])
+        
+    nodeinfo.sort(key = lambda x : x[1], reverse=True)
+    
+    BT = binaryTree()
+    
+    for i in nodeinfo:
+        n = Node(i[0], i[1], i[2])
+        if BT.root == None:
+            BT.root = n
+        else:
+            BT.insertNode(BT.root, n)
+    
     tmp = []
-
-    for i in range(n):
-        tmp.append([nodeinfo[i][0], nodeinfo[i][1], i+1])
+    BT.preorder(BT.root)
+    answer.append(tmp)
+    tmp = []
+    BT.postorder(BT.root)
+    answer.append(tmp)
     
-    tmp = sorted(tmp, key=lambda x:x[1], reverse=True)
-    
-    for i in range(n):
-        for j in range(n):
-            #if graph[i][1] 
-    
-    print(tmp)
-    
-
     return answer
-
-nodeinfo = [[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]
-solution(nodeinfo)
-    
