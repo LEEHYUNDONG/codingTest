6e7cf7de66effd73968d5275196a30a7833bb8a7
LEEHYUNDONG <l.steve10@yahoo.com>
2022-02-22 21:01:15 +0900

    숨바꼭질3

--- codingTest_python/BOJ/hideNSeek.py
+++ codingTest_python/BOJ/hideNSeek.py
@@ -1,50 +1,22 @@
-# from collections import deque
-
-# n, m = map(int, input().split())
-# q = deque([])
-# q.append([n, 0])
-# INF = int(1e9)
-# visited = [INF] * max(m+1, n+1)
-
-# while q:
-#     loc, time = q.popleft()
-#     if loc == m:
-#         print(visited[loc])
-#         break
-#     if loc*2 <= m:
-#         q.appendleft([2*loc, time])
-#         visited[2*loc] = min(time, visited[2*loc])
-#     if 1 <= loc <= m-1:
-#         q.append([loc+1, time+1])
-#         q.append([loc-1, time+1])
-#         visited[loc-1] = min(time+1, visited[loc-1])
-#         visited[loc+1] = min(time+1, visited[loc+1])
-
-
 from collections import deque
-MAX = 100001
-check = [False] * MAX
-dist = [-1] * MAX
 
-n,k = map(int, input().split())
-q = deque()
+n, m = map(int, input().split())
+q = deque([])
 q.append(n)
-check[n] = True
-dist[n] = 0
+visited = [-1] * 200001
+visited[n] = 0
 
 while q:
-    now = q.popleft()
-    if now*2 <= MAX and check[now*2] == False:  # 순간이동
-        q.appendleft(now*2)
-        check[now*2] = True
-        dist[now*2] = dist[now]
-    if now + 1 <= MAX and check[now+1] == False: # x+1이동
-        q.append(now+1)
-        check[now+1] = True
-        dist[now+1] = dist[now] + 1
-    if now - 1 >= 0 and check[now-1] == False: # x-1이동
-        q.append(now-1)
-        check[now-1] = True
-        dist[now-1] = dist[now] + 1
-
-print(dist[k])
+    loc = q.popleft()
+    if loc == m:
+        print(visited[loc])
+        break
+    if loc*2 <= 200000 and visited[2*loc] == -1:
+        q.appendleft(2*loc)
+        visited[2*loc] = visited[loc]
+    if 0 <= loc-1 and visited[loc-1] == -1:
+        q.append(loc-1)
+        visited[loc-1] = visited[loc]+1
+    if loc+1 <= 200000 and visited[loc+1] == -1:
+        q.append(loc+1)
+        visited[loc+1] = visited[loc]+1
