a35daeaf6f533ec8ae31763ce3c50d14c3dbd172
LEEHYUNDONG <l.steve10@yahoo.com>
2022-03-10 11:33:30 +0900

    N AND M 12

---
+++ codingTest_python/BOJ/nAndM12.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+arr = list(map(int, input().split()))
+arr.sort()
+visited = [False for _ in range(n)]
+ans = set()
+
+def dfs(idx, tmp, depth):
+    global ans
+    if depth == m:        
+        
+        ans.add(tuple(tmp))
+        return
+    
+    for i in range(idx, n):
+        tmp.append(arr[i])
+        dfs(i, tmp, depth+1)
+        tmp.pop(-1)
+        
+
+
+dfs(0, [], 0)
+
+ans = list(ans)
+ans.sort()
+
+for i in ans:
+    print(*i)
