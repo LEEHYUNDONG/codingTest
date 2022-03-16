0ec4d033c6bb1661e38be5f210f02ba7fd0d50d8
LEEHYUNDONG <l.steve10@yahoo.com>
2022-03-16 11:07:57 +0900

    보석 쇼핑

--- codingTest_python/programmers/jewelShopping.py
+++ codingTest_python/programmers/jewelShopping.py
@@ -1,16 +1,27 @@
 def solution(gems):
-    answer = [1, 100000]
-    size = len(set(gems))
+    answer = [1, 1000000]
+    size = len(gems)
+    msize = len(set(gems))
     l, r = 0, 0
-    while l <= r:
-        tmp = []
-        for i in range(l, r):
-            tmp.append(gems[i])
-        if size == len(set(tmp)) and (answer[1] - answer[0]) > (r - l):
-            answer = [l+1, r]
-        if size <= len(tmp):
-            l += 1
-        elif size > len(tmp) and r < len(gems):
-            r += 1
-        
+    gemsDict = dict()
+    ans = len(gems) + 1
+
+    while r < size:
+        if gems[r] not in gemsDict:
+            gemsDict[gems[r]] = 1
+        else:
+            gemsDict[gems[r]] += 1
+        r += 1
+        if len(gemsDict) == msize:
+            while l < r:
+                if gemsDict[gems[l]] > 1:
+                    gemsDict[gems[l]] -= 1
+                    l += 1
+                elif r - l < ans:
+                    ans = r - l
+                    answer = [l+1, r]
+                    break
+                else:
+                    break
+
     return answer
