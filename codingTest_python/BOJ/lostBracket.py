bf94243205c90efb88e5c0e04ffd22a46b405344
LEEHYUNDONG <l.steve10@yahoo.com>
2022-03-29 21:48:47 +0900

    잃어버린 괄호

--- codingTest_python/BOJ/lostBracket.py
+++ codingTest_python/BOJ/lostBracket.py
@@ -1,19 +1,15 @@
 import re
-
+import sys
+input = sys.stdin.readline
 exp = input().strip()
 
 
 num = re.findall('[\d]+', exp)
 op = re.findall('[\D]', exp)
 
-if len(op) == 1:
-    if op == '+':
-        print(int(num[0])+int(num[-1]))
-    else:
-        print(int(num[0])-int(num[-1]))
-
 flag = False
 ans = 0
+
 for i in range(len(num)):
     if not flag:
         ans += int(num[i])
@@ -22,4 +18,5 @@ for i in range(len(num)):
     if i < len(num) - 1:
         if op[i] == '-':
             flag = True
+
 print(ans)
