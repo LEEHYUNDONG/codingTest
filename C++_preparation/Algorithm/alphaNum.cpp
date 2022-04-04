21bb7ac4af84f37fdaec371ed4cf21d7dbeda58e
LEEHYUNDONG <l.steve10@yahoo.com>
2022-04-04 15:51:46 +0900

    알파벳 개수

---
+++ C++_preparation/Algorithm/alphaNum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int arr[26] = {0};
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        arr[s[i] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    return 0;
+}
