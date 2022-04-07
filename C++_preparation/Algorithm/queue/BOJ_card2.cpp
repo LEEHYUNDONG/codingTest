83f19fe602971018a013e223b146043af1802159
LEEHYUNDONG <l.steve10@yahoo.com>
2022-04-07 14:57:33 +0900

    카드2

---
+++ C++_preparation/Algorithm/queue/BOJ_card2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    queue<int> q;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        q.push(i + 1);
+
+    while (q.size() > 1)
+    {
+        q.pop();
+        int tmp = q.front();
+        q.pop();
+        q.push(tmp);
+    }
+    cout << q.front();
+
+    return 0;
+}
