9acc4e1130d3d280cb07e8b76896f61e1228fa05
LEEHYUNDONG <l.steve10@yahoo.com>
2022-05-30 12:36:28 +0900

    가장 긴 증가하는 부분 수열 5

--- C++_preparation/BOJ/12738.cpp
+++ C++_preparation/BOJ/12738.cpp
@@ -2,27 +2,52 @@
 using namespace std;
 typedef long long ll;
 
-
 ll N;
-vector<ll> arr(1000010);
+vector<ll> arr;
 vector<ll> dp;
-
+vector<ll> f;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> N;
-    for (int i = 0; i < N;i++)
-        cin >> arr[i];
-
-    for (int i = 0; i < N;i++){
-        auto it = lower_bound(dp.begin(), dp.end(), arr[i]);
-        if(dp.size() == 0||it == dp.end())
-            dp.push_back(arr[i]);
-        else
-            dp[it - dp.begin()] = arr[i];
+    ll size = 0;
+    for (int i = 0; i < N; i++)
+    {
+        ll tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+        auto it = lower_bound(dp.begin(), dp.end(), tmp);
+        f.push_back(it-dp.begin());
+        if (it == dp.end())
+        {
+            dp.push_back(tmp);
+            size++;
+        }
+        else{
+            dp[it-dp.begin()] = tmp;
+            
+        }
     }
-    cout << dp.size();
+    vector<ll> ans;
+    for (int i = N - 1; i > -1; i--)
+    {
+        if (size < 1)
+            break;
+        if(f[i] == size-1){
+            ans.push_back(arr[i]);
+            size--;
+        }
+
+    }
+
+    cout << ans.size() << '\n';
+    while(!ans.empty()){
+        cout << ans.back() << ' ';
+        ans.pop_back();
+    }
     return 0;
 }
+// 9
+// 3 1 4 6 2 2 0 3 6
