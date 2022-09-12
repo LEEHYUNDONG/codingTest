5cfd9b9e080b0ff23dc143ff462e3ca322cd58c8
LEEHYUNDONG <l.steve10@yahoo.com>
2022-09-12 16:38:31 +0900

    두 큐의 합

--- C++_preparation/Programmers/Programmers_twoQ.cpp
+++ C++_preparation/Programmers/Programmers_twoQ.cpp
@@ -1,9 +1,48 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
+typedef long long ll;
 
 int solution(vector<int> queue1, vector<int> queue2) {
-    int answer = -2;
+    ll answer = 0;
+    ll tot = 0, tot1 = 0, tot2 = 0;
+    ll l = queue1.size();
+    queue<ll> q1, q2;
+    
+    for(auto a : queue1){
+        q1.push(a);
+        tot1 += a;
+    }
+    for(auto a : queue2){
+        q2.push(a);
+        tot2 += a;
+    }
+    
+    
+    while(tot1 != tot2){
+        if(answer >= l*3){
+            answer = -1;
+            break;
+        }
+            
+        if(tot1 < tot2){
+            tot1 += q2.front();
+            tot2 -= q2.front();
+            q1.push(q2.front());
+            q2.pop();
+            
+        }else if(tot1 > tot2){
+            tot2 += q1.front();
+            tot1 -= q1.front();
+            q2.push(q1.front());
+            q1.pop();
+        }
+        answer++;
+    }
+    
     return answer;
 }
