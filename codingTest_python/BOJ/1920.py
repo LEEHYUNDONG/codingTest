8b1a71be8b676a6d45ad4bedb943a179c211f6a3
LEEHYUNDONG <l.steve10@yahoo.com>
2022-09-18 21:05:47 +0900

    카드게임

---
+++ codingTest_python/BOJ/1920.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+targets = list(map(int, input().split()))
+
+arr.sort()
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return 1
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
+
+for target in targets:
+    print(binary_search(arr, target, 0, N-1))
