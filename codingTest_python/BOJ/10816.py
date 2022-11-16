3fa891da20497e61ce1c793b43855f1a80429c5c
LEEHYUNDONG <l.steve10@yahoo.com>
2022-11-16 21:31:53 +0900

    카드게임

--- codingTest_python/BOJ/10816.py
+++ codingTest_python/BOJ/10816.py
@@ -1,22 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-
 N = int(input())
 arr = list(map(int, input().split()))
 M = int(input())
 targets = list(map(int, input().split()))
 
 arr.sort()
-
+count = {}
+for a in arr:
+    if a not in count:
+        count[a] = 1
+    count[a] += 1
 
 
 def binary_search(array, target, start, end):
     if start > end:
         return 0
     mid = (start + end) // 2
+    
     if array[mid] == target:
-        return array.count(target)
+        return count.get(target)
     elif array[mid] > target:
         return binary_search(array, target, start, mid-1)
     else:
