f00b3a91b0a16ba7d38d73b90e2f14316bb7c62b
LEEHYUNDONG <hd.lee1@cj.net>
2023-01-03 15:17:18 +0900

    두 동전

---
+++ codingTest_python/BOJ/16197.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = [list(input().rstrip()) for _ in range(N)]
+
+
+def bfs(x, y):
+    visited = [[False]*M for _ in range(N)]
+    q = deque([])
+    q.append(())
+    
