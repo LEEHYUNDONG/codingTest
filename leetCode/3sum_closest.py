11f3f54e9678c278d8709a5def63c66f27069d1c
LEEHYUNDONG <hd.lee1@cj.net>
2023-02-28 10:18:38 +0900

    3 sum closest - two pointer

---
+++ leetCode/3sum_closest.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        clso = 10000000
+        res = 0
+        for ptr_1 in range(len(nums)-2):
+            ptr_2, ptr_3 = ptr_1 + 1, len(nums)-1
+            cur_target = target - nums[ptr_1]
+            while ptr_2 < ptr_3:
+                s = nums[ptr_2] + nums[ptr_3]
+                if abs(cur_target - s) < clso:
+                    clso = abs(cur_target - s)
+                    res = s + nums[ptr_1]
+                if cur_target < s:
+                    ptr_3 -= 1
+                elif cur_target > s:
+                    ptr_2 += 1
+                else:
+                    return target
+        return res
+        
