4a68fd0f94f71edb0cfaf99566d35f219fa06a6e
LEEHYUNDONG <l.steve10@yahoo.com>
2023-05-20 11:59:05 +0900

    두 원 사이의 정수

--- codingTest_python/programmers/twoCircle.py
+++ codingTest_python/programmers/twoCircle.py
@@ -1,10 +1,19 @@
-import math
-
 def solution(r1, r2):
-    inner_dot_num = 0
-    for x in range(1, r2 + 1):
-        y_max = math.floor(math.sqrt(r2**2 - x**2))
-        y_min = 0 if x >= r1 else math.ceil(math.sqrt(abs(r1**2 - x**2)))
-        inner_dot_num += y_max - y_min + 1
+    answer = 0
     
-    return inner_dot_num * 4
+    # 1사 분면 위의 점만 계산 하여 4배
+    cnt = 0
+    y_min, y_max = r1, r2
+    
+    for x in range(0, r2):
+        # max를 줄여서 범위를 줄인다.
+        while r2**2 < y_max**2 + x**2:
+            y_max -= 1
+        
+        # min을 줄여서 범위를 늘린다.
+        while y_min-1 and r1**2 <= (y_min-1)**2 + x**2:
+            y_min -= 1
+        cnt += y_max - y_min + 1
+        # print("x, y_max, y_min", x, y_max, y_min)
+        
+    return cnt*4
