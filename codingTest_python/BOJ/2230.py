cb0e029c2a7c93d5a44ffaafdec78a40ca0671db
LEEHYUNDONG <l.steve10@yahoo.com>
2023-05-29 15:10:03 +0900

    수 고르기

---
+++ codingTest_python/BOJ/2230.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+
+l, r = 0, 0
+arr.sort() #정렬해야함.
+ans = max(arr) - min(arr)
+
+# 커서가 왼쪽과 오른쪽이 같이 시작한다
+while l <= r and r < N:
+    
+    if arr[r] - arr[l] >= M:
+        ans = min(ans, arr[r]-arr[l])
+        l += 1
+    else:
+        r += 1
+print(ans)
