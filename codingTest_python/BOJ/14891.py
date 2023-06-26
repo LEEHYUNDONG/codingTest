bde09c01f997a7b616d354ff270141308f665ad9
LEEHYUNDONG <l.steve10@yahoo.com>
2023-06-26 15:58:45 +0900

    톱니바퀴

--- codingTest_python/BOJ/14891.py
+++ codingTest_python/BOJ/14891.py
@@ -1,13 +1,39 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+gear = [deque(list(input())) for _ in range(4)]
+k = int(input())
 
-tob = []
-for _ in range(N):
-    tmp = list(map(int, input().split()))
-    tob.append(deque(tmp))
 
-N = int(input())
-test = [list(map(int, input().split())) for _ in range(N)]
 
+for _ in range(k):
+    gear_num, wise = map(int, input().split())
+    gear_num -= 1
+    tmp = wise
+    left, right = gear[gear_num][6], gear[gear_num][2] # 원래 왼쪽 오른쪽 비교해서 확장
+    
+    # 왼쪽 방향
+    tmp = wise
+    for i in range(gear_num-1, 0-1, -1):
+        if gear[i][2] != left:    
+            left = gear[i][6]
+            gear[i].rotate(-1*tmp)
+            tmp *= -1
+        else:
+            break
+    # 오른쪽 방향
+    tmp = wise
+    for i in range(gear_num+1, 4):
+        if gear[i][6] != right:    
+            right = gear[i][2]
+            gear[i].rotate(-1*tmp)
+            tmp *= -1
+        else:
+            break
+    gear[gear_num].rotate(wise)
+
+ans=0
+for i in range(4):
+    if gear[i][0]=='1':
+        ans += (2**i)
+print(ans)
